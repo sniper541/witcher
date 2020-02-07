782b042ffd83d82bf27e3bc766b33e905bf446c8
sniper541 <42844548+sniper541@users.noreply.github.com>
2020-02-07 20:36:22 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView:1,
+  loop:true,
+  navigation:{
+    nextEl: '.arrow',
+  },
+  breakpoints: {
+  
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    }
+  }
+});
+
+let menuButton = document.querySelector('.menu-button');
+let menu = document.querySelector('.header');
+menuButton.addEventListener('click', function (){
+  menuButton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
+})
+
+
